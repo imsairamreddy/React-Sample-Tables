ca07a5cd427628ac74d0028954b25176ed907f42
imsairamreddy <sboddire@my.bridgeport.edu>
2024-01-22 12:02:40 -0600

    React sample table implementation

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import CatFactsTable from "./components/CatFactsTable";
+
+const App = () => {
+  return (
+      <div>
+        <CatFactsTable/>
+      </div>
+  );
+};
+
+export default App;
